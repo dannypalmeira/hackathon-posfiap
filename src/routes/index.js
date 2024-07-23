126abd7a24cf9e350624e28f3b798cc585b589be
Adorno Felipe <felipeoliveiraadorno77@hotmail.com>
2024-07-22 23:39:14 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ import ong from "./ongs.js";
 
 const routes = (app) => {
     app.route("/").get((req, res) => res.status(200).send
-    ("Curso de Node.js")
+    ("Hacka")
 );
     app.use(express.json(), usuario , ong);
 
